7d5f9e0b7f8bd5e292b15ee33f1508251afad0ae
yr5 <39791273+yr5@users.noreply.github.com>
2018-07-24 01:35:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ console.log("The Welcome Is ready");
 client.on('message', message => {
       var ms = 120000;
         let timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  let words = [`****الله اكبر**`,`**الحمدالله**`,`**سبحان الله`]
+  let words = [`الله اكبر`,`الحمدالله`,`سبحان الله`]
 if (message.content === "اجر") {
   
        setInterval (function () {
-          message.channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+          message.channel.send(`**${words[Math.floor(Math.random() * words.length)]}**`) 
        
       }, ms); 
     }
